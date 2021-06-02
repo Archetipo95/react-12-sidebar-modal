255780b55bfceefab6cd43e641d713bf402800e4
Martin Masevski <martin.masevski@gmail.com>
2021-06-02 21:30:24 +0200

    Implemented

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-12-sidebar-modal
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/d030a0a2-10a5-46ca-add8-b683e58ab300/deploy-status)](https://app.netlify.com/sites/flamboyant-swartz-16d590/deploys)
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,20 @@
-import React, { useContext } from 'react'
-import { FaBars } from 'react-icons/fa'
-
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import { useMyHook } from "./context";
 const Home = () => {
-  return <h2>home component</h2>
-}
+  // const data = useContext(AppContex)
+  // const data = useMyHook();
 
-export default Home
+  const { openSidebar, openModal } = useMyHook();
+
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button className="btn" onClick={openModal}>show modal</button>
+    </main>
+  );
+};
+
+export default Home;
